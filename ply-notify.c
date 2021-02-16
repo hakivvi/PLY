98deac4cdb459de234ba7091cab10f15d25f90c0
Hakim <67718634+hakivvi@users.noreply.github.com>
2021-02-16 21:01:54 +0100

    add comments

--- ply-notify.c
+++ ply-notify.c
@@ -1,5 +1,11 @@
 // compile: gcc ply-notify.c $(pkg-config --libs --cflags dbus-1) -o ply-notify
 // run: ply-notify "PLY" "PLY has started"
+
+// we cannot use "dbus-send" to deliver our message, because "Notify" method has a required argument of type "dict of variant" :
+// <arg type="a{sv}" name="arg_6" direction="in"> </arg> // arg_6 = hints
+// and "dbus-send" does not support this type of arguments, so we have to use our own program
+// Note: "gdbus" does support this type of datatype.
+
 #include <dbus/dbus.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -38,7 +44,7 @@ int main(int argc, char* argv[]) {
 	dbus_message_set_destination(dbus_message, INTERFACE);
 
 	if ( !build_message(dbus_message, argv[1], argv[1], argv[2]) ) {
-		fprintf(stderr, "Error while building message: Out of memory");
+		fprintf(stderr, "Error while building message: Out Of Memory");
 		return 1;
 	}
 
@@ -46,7 +52,7 @@ int main(int argc, char* argv[]) {
 
 	// send message and block until reply (for a default timeout)
 	// i could use dbus_pending_call_set_notify() with a callback but :P
-	// *dbus_message holds now the reply (NULL for error)
+	// dbus_message holds now the reply (NULL for error)
 	dbus_error_init(&dbus_error);
 	dbus_message = dbus_connection_send_with_reply_and_block(dbus_connection, dbus_message, -1, &dbus_error);
 	check_for_error(CHECK_IS_MSG_SENT, dbus_message);
@@ -95,27 +101,27 @@ dbus_bool_t build_message(DBusMessage* message, char *app_name, char *summary, c
 	/*
 	Notify method signature:
 
-	UINT32 org.freedesktop.Notifications.Notify (
-	    STRING app_name,
-	    UINT32 replaces_id, 
-	    STRING app_icon, 
-	    STRING summary, 
-	    STRING body, 
-	    ARRAY actions, 
-	    DICT hints, 
-	    INT32 expire_timeout
+	UINT32 org.freedesktop.Notifications.Notify ( 	<- <arg type="u" name="arg_8" direction="out"></arg> (return value)
+	    STRING app_name,      			-> <arg type="s" name="arg_0" direction="in"></arg>
+	    UINT32 replaces_id,  			-> <arg type="u" name="arg_1" direction="in"></arg>
+	    STRING app_icon,     			-> <arg type="s" name="arg_2" direction="in"></arg>
+	    STRING summary,     			-> <arg type="s" name="arg_3" direction="in"></arg>
+	    STRING body,         			-> <arg type="s" name="arg_4" direction="in"></arg>
+	    ARRAY actions,       			-> <arg type="as" name="arg_5" direction="in"></arg>
+	    DICT hints,          			-> <arg type="a{sv}" name="arg_6" direction="in"></arg>
+	    INT32 expire_timeout 			-> <arg type="i" name="arg_7" direction="in"></arg>
         );
 	*/
 	
 	DBusMessageIter arguments;
 	DBusMessageIter iterators[3];
 
-	int replaces_id = 1337;
-	char *app_icon[] = {""};
-	char* actions[] = { NULL };
-	char* hints_key = "key";
-	int hints_value = *(int*)"value";
-	signed int timeout = -1;
+	int replaces_id = 1337; // unsigned int
+	char * app_icon = ""; // string
+	char * actions[] = { NULL }; // array of strings
+	char * hints_key = "foo"; // string
+	char * hints_value = "bar"; // this actaully can be pretty much anything that can be wrapped in a variant
+	signed int timeout = -1; // signed int
 
 	dbus_bool_t success = 1;
 	
@@ -137,19 +143,22 @@ dbus_bool_t build_message(DBusMessage* message, char *app_name, char *summary, c
 	// you have to use an iterator rather than dbus_message_iter_append_basic()
 
 	// So here we go:
-	// append actions[] (array) :
+	// append actions[] (array of strings) :
 	success &= dbus_message_iter_open_container(&arguments, DBUS_TYPE_ARRAY, DBUS_TYPE_STRING_AS_STRING, &iterators[0]);
 	for (int i = 0; actions[i]; i++ )
 		success &= dbus_message_iter_append_basic(&iterators[0], DBUS_TYPE_STRING, &actions[i]);
 	success &= dbus_message_iter_close_container(&arguments, &iterators[0]);
 
-	// append hints (dictionary datatype {"key":"value"})
-	success &= dbus_message_iter_open_container(&arguments, DBUS_TYPE_ARRAY, "{sv}", &iterators[0]); // {sv} is the signature for dict type: {String, Variant}
-	success &= dbus_message_iter_open_container(&iterators[0], DBUS_TYPE_DICT_ENTRY, NULL, &iterators[1]); 
-	success &= dbus_message_iter_append_basic(&iterators[1], DBUS_TYPE_STRING, &hints_key); // dict entry is the key element in the dict
-	success &= dbus_message_iter_open_container(&iterators[1], DBUS_TYPE_VARIANT, DBUS_TYPE_INT32_AS_STRING, &iterators[2]); // next is "value"
-	success &= dbus_message_iter_append_basic(&iterators[2], DBUS_TYPE_INT32, &hints_value);
-	success &= dbus_message_iter_close_container(&iterators[1], &iterators[2]);
+	// append hints (dictionary datatype {"key":<"value">})
+	success &= dbus_message_iter_open_container(&arguments, DBUS_TYPE_ARRAY, "{sv}", &iterators[0]); // open a container for dict (string: variant)
+	
+	success &= dbus_message_iter_open_container(&iterators[0], DBUS_TYPE_DICT_ENTRY, NULL, &iterators[1]); // open a container for the dict entry / key (type: string)
+	success &= dbus_message_iter_append_basic(&iterators[1], DBUS_TYPE_STRING, &hints_key); // append "foo" (string)
+	
+	success &= dbus_message_iter_open_container(&iterators[1], DBUS_TYPE_VARIANT, DBUS_TYPE_STRING_AS_STRING, &iterators[2]); // open a container for value (type: variant)
+	success &= dbus_message_iter_append_basic(&iterators[2], DBUS_TYPE_STRING, &hints_value); // append "bar" (string)
+	
+	success &= dbus_message_iter_close_container(&iterators[1], &iterators[2]); // close opened containers
 	success &= dbus_message_iter_close_container(&iterators[0], &iterators[1]);
 	success &= dbus_message_iter_close_container(&arguments, &iterators[0]); 
 	
